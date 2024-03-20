3da104492c4a1de5fce8674d52b40918fa5f784a
JakubCe15 <162459302+JakubCe15@users.noreply.github.com>
2024-03-20 13:32:16 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+  String hello = ("Hello World");
+    for (int i = 0; i < hello.length(); i++){
+      System.out.println(hello.charAt(i));
+  }
+ }
+}
